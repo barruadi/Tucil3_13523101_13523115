b67b4e75521bbf95cae7afce5466a32d0920bf47
azfaradhi <aradhihakim@gmail.com>
2025-05-17 18:54:43 +0700

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import board.Board;
 import java.util.*;
 import utils.InputOutput;
+import algorithm.AStar;
 
 public class Main {
 
@@ -7,8 +9,25 @@ public class Main {
 
         InputOutput io = new InputOutput();
         List<String> lines = io.readFileFromString("tes.txt");
-        for (String line : lines) {
-            System.out.println(line);
+        // for (String line : lines) {
+        //     System.out.println(line);
+        // }
+        Board board = io.makeBoard(lines);
+        io.makeBlocks(lines,board);
+        board.printBoard();
+        List<Board> allPossibleMoves = board.getAllPossibleMove();
+        AStar aStar = new AStar();
+        List<Board> boards = aStar.AStarAlgorithm(board);
+        int count = 0;
+        if (boards != null) {
+            System.out.println("Found solution:" + boards.size());
+            for (Board b : boards) {
+                System.out.println("Step " + count++);
+                b.printBoard();
+            }
+        } else {
+            System.out.println("No solution found.");
         }
+        
     }
 }
