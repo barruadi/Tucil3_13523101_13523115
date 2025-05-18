cff13ce5c0edaa28b14988258b1064f05b445ba8
azfaradhi <aradhihakim@gmail.com>
2025-05-19 06:37:53 +0700

    fix return type

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import algorithm.AStar;
 import board.Board;
 import java.util.*;
 import utils.InputOutput;
-import algorithm.AStar;
 
 public class Main {
 
@@ -9,12 +9,14 @@ public class Main {
 
         InputOutput io = new InputOutput();
         List<String> lines = io.readFileFromString("tes.txt");
-        // for (String line : lines) {
-        //     System.out.println(line);
-        // }
+
         Board board = io.makeBoard(lines);
-        io.makeBlocks(lines,board);
-        board.printBoard();
+        boolean cek = io.makeBlocks(lines,board);
+        if (!cek) {
+            System.out.println("Data yang dimasukkan tidak valid");
+            return;
+        }
+
         List<Board> allPossibleMoves = board.getAllPossibleMove();
         AStar aStar = new AStar();
         List<Board> boards = aStar.AStarAlgorithm(board);
